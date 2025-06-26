294850852e4c9d5eb41b84dcdb47d1e07fd9a323
Ctrian <andrescalvache47@gmail.com>
2025-06-26 15:28:37 -0500

    Tarea 8

--- src/main/java/uce/edu/web/api/controller/ProfesorController.java
+++ src/main/java/uce/edu/web/api/controller/ProfesorController.java
@@ -1,7 +1,15 @@
 package uce.edu.web.api.controller;
 
+import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import uce.edu.web.api.repository.modelo.Profesor;
@@ -14,8 +22,39 @@ public class ProfesorController {
     private IProfesorService profesorService;
 
     @GET
-    @Path("/consultar/{id}")
+    @Path("/{id}")
     public Profesor consultarProfesorPorId(@PathParam("id") Integer id) {
         return this.profesorService.buscarPorId(id);
     }
+
+    @GET
+    @Path("")
+    public List<Profesor> consultarProfesores() {
+        return this.profesorService.buscarTodos();
+    }
+
+    @POST
+    @Path("")
+    public void guardar(Profesor profesor){
+
+    }
+
+    @PUT
+    @Path("")
+    public void actualizar(@RequestBody Profesor profesor, @PathParam("id") Integer id){
+
+    }
+
+    @PATCH
+    @Path("/{id}")
+    public void actualizarParcial(@RequestBody Profesor profesor, @PathParam("id") Integer id) {
+
+    }
+
+    @DELETE
+    @Path("{/id}")
+    public void eliminar(@PathParam("id") Integer id) {
+        
+    }
+
 }
