dda9c1a5806e022871faa33c8e99cf9140e95e8f
Ctrian <andrescalvache47@gmail.com>
2025-06-25 22:12:42 -0500

    Taller 30

--- src/main/java/uce/edu/web/api/controller/EstudianteController.java
+++ src/main/java/uce/edu/web/api/controller/EstudianteController.java
@@ -1,7 +1,15 @@
 package uce.edu.web.api.controller;
 
+import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import uce.edu.web.api.repository.modelo.Estudiante;
@@ -14,8 +22,39 @@ public class EstudianteController {
     private IEstudianteService estudianteService;
 
     @GET
-    @Path("/consultar/{id}")
+    @Path("/{id}")
     public Estudiante consultarEstudiantePorId(@PathParam("id") Integer id) {
         return this.estudianteService.buscarPorId(id);
+    }
+
+    @GET
+    @Path("")
+    public List<Estudiante> consultarEstudiantes() {
+        return this.estudianteService.buscarTodos();
+    }
+
+    @POST
+    @Path("")
+    public void guardar(Estudiante estudiante) {
+       
+    }
+ 
+    @PUT
+    @Path("/{id}")
+    public void actualizar(@RequestBody Estudiante estudiante, @PathParam("id") Integer id) {
+       
+ 
+    }
+ 
+    @PATCH
+    @Path("/{id}")
+    public void actualizarParcial(@RequestBody Estudiante estudiante, @PathParam("id") Integer id) {
+ 
+ 
+    }
+ 
+    @DELETE
+    @Path("/{id}")
+    public void eliminar(@PathParam("id") Integer id) {
     }
 }
--- src/main/java/uce/edu/web/api/repository/EstudianteRepoImp.java
+++ src/main/java/uce/edu/web/api/repository/EstudianteRepoImp.java
@@ -1,8 +1,11 @@
 package uce.edu.web.api.repository;
 
+import java.util.List;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import uce.edu.web.api.repository.modelo.Estudiante;
 
@@ -18,5 +21,11 @@ public class EstudianteRepoImp implements IEstudianteRepo {
     public Estudiante seleccionarPorId(Integer id) {
         return this.entityManager.find(Estudiante.class, id);
 
+    }
+
+    @Override
+    public List<Estudiante> seleccionarTodos() {
+        TypedQuery<Estudiante> myQuery = this.entityManager.createQuery("SELECT e FROM Estudiante e", Estudiante.class);
+        return myQuery.getResultList();
     }
 }
